c3674d06b7dae3201cb168ba53fb1d4766af4509
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-10-17 19:17:04 +0300

    Excel reader + unzipper

---
+++ ExcelDataReader/ExcelSheetReader.cs
@@ -0,0 +1,66 @@
+﻿namespace ExcelReportLoader
+{
+    using System;
+    using System.Data;
+    using System.Data.OleDb;
+
+    public class ExcelSheetReader
+    {
+        internal const string ConnectionStringFormat = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES\";";
+        internal const string QueryFormat = "SELECT * FROM [{0}]";
+        internal const string DefaultSheetName = "TABLE_NAME";
+
+        public ExcelSheetReader()
+        {
+        }
+
+        /// <summary>
+        /// Read an excel worksheet in an excel file specified by the provided file path and executes the provided action on every read of the DataTableReader(a reader for every row)>
+        /// </summary>
+        /// <param name="excelFilePath">The absolute path of the that the reader will read from.</param>
+        /// <param name="sheetName">The string representation of the name of the sheet to read from.</param>
+        /// <param name="action">The action to execute with parameters the excel sheets as an array.</param>
+        public void ReadExcelDataWithAction(string excelFilePath, string sheetName, Action<DataTableReader> action)
+        {
+            var connectionString = string.Format(ConnectionStringFormat, excelFilePath);
+
+            using (var connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                string sheet;
+
+                if (string.IsNullOrWhiteSpace(sheetName))
+                {
+                    var dataSheet = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    sheet = dataSheet.Rows[0][DefaultSheetName].ToString();
+                }
+                else
+                {
+                    sheet = sheetName.EndsWith("$") ? sheetName : sheetName + "$";
+                }
+
+                var oleCommand = new OleDbCommand(QueryFormat.Formatted(sheet), connection);
+
+                using (var adapter = new OleDbDataAdapter(oleCommand))
+                {
+                    using (var dataSet = new DataSet())
+                    {
+                        adapter.Fill(dataSet);
+
+                        using (var reader = dataSet.CreateDataReader())
+                        {
+                            while (reader.Read())
+                            {
+                                action(reader);
+                            }
+                        }
+                    }
+                }
+
+                oleCommand = null;
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ ExcelDataReader/Unzipper.cs
@@ -0,0 +1,39 @@
+﻿namespace ExcelReportLoader
+{
+    using Ionic.Zip;
+
+    public class Unzipper
+    {
+        public Unzipper()
+        {
+        }
+
+        public Unzipper(string zipFilePath, ExtractExistingFileAction extractionStratedy = ExtractExistingFileAction.OverwriteSilently)
+        {
+            this.Path = zipFilePath;
+            this.ExtractionStrategy = extractionStratedy;
+        }
+
+        public string Path { get; set; }
+
+        public ExtractExistingFileAction ExtractionStrategy { get; set; }
+
+        public void Extract(string destinationFolderPath)
+        {
+            using (var zip = new ZipFile(this.Path))
+            {
+                zip.ExtractAll(destinationFolderPath, this.ExtractionStrategy);
+            }
+        }
+
+        public ZipInputStream Extract(ZipInputStream extractionStream)
+        {
+            using (var zip = new ZipFile(this.Path))
+            {
+                zip.ForEach(x => x.Extract(extractionStream));
+            }
+
+            return extractionStream;
+        }
+    }
+}
